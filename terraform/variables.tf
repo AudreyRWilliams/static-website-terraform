cd1e93b48e447847514113237a21dbd30278c8f9
Audrey Williams <120662264+AudreyRWilliams@users.noreply.github.com>
2025-08-27 01:00:06 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Global unique bucket name (e.g. my-unique-site-12345)"
+  type        = string
+}
 
